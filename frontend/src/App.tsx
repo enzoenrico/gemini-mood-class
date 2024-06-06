ba3d5df25b09476525d90a321966abedc35e93e7
Enzo Enrico <47016293+enzoenrico@users.noreply.github.com>
2024-06-06 03:40:53 +0000

    que sono

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,9 @@ function App() {
   const [word, setWord] = useState("");
   const [data, setData] = useState("");
 
-  const fetchData = () => {
+  const but = document.querySelector<HTMLButtonElement>("#but");
+
+  const fetchData = async ():Promise<object> => {
     if (word.length > 0) {
       fetch("http://localhost:5000/classify/" + word, {
         method: "GET",
@@ -13,17 +15,26 @@ function App() {
         application: "application/json",
       })
         .then((res) => res.json())
-        .then((data) => {
+        .then((data: JSON) => {
           console.log(data);
+          setData(data["color"]);
+        })
+        .catch((err) => {
+          console.log(err);
+          return;
         });
     }
   };
 
-  document.addEventListener("keydown", (e) => {
-    if (e.key == "Enter") {
-      fetchData();
-    }
-  });
+  const setBut = () => {
+    console.log("data:" + data)
+    but.innerText = data;
+    but.style.backgroundColor = data;
+  };
+
+  const handleFetch = () => {
+    fetchData()
+  };
 
   return (
     <>
@@ -34,7 +45,7 @@ function App() {
           value={word}
           onChange={(e) => setWord(e.target.value)}
         />
-        <button id="but" onClick={fetchData}>
+        <button id="but" onClick={handleFetch}>
           Fetch Address
         </button>
         <p>{data}</p> {/* Render the API response */}
